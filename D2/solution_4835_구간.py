b6b3904d1f200a65b23a0b4340c5bf9cce221058
GAEUN JEONG <rkdms357@naver.com>
2025-04-09 11:40:15 +0900

    swea 4835 구간합

---
+++ D2/solution_4835_구간.py
@@ -0,0 +1,14 @@
+T = int(input())
+for test_case in range(1, T+1):
+    N, M = map(int, input().split())
+    lis = list(map(int, input().split()))
+    mx = 0
+    mn = 10000 * N
+    sm = 0
+    for i in range(0, N-M+1):
+        sm = sum(lis[i:i+M])
+        if sm < mn:
+            mn = sm
+        if sm > mx:
+            mx = sm
+    print(f'#{test_case}', mx-mn)
