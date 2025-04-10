7a93796f6c0fccf9fe2f0a4bffd7d3e9784e6fd1
GAEUN JEONG <rkdms357@naver.com>
2025-04-10 16:30:26 +0900

    swea 1954 달팽이 숫자

---
+++ D2/solution_1954_달팽이숫자.py
@@ -0,0 +1,20 @@
+T = int(input())
+di = [0, 1, 0, -1]
+dj = [1, 0, -1, 0]
+for test_case in range(1, T+1):
+    N = int(input())
+    arr = [[0] * N for _ in range(N)]
+    i, j, cnt, dr = 0, 0, 1, 0
+    arr[i][j] = cnt
+    cnt += 1
+    while cnt <= N * N:
+        ni, nj = i + di[dr], j + dj[dr]
+        if 0 <= ni < N and 0 <= nj < N and arr[ni][nj] == 0:
+            i, j = ni, nj
+            arr[i][j] = cnt
+            cnt += 1
+        else:
+            dr = (dr + 1) % 4
+    print(f'#{test_case}')
+    for lis in arr:
+        print(*lis)
